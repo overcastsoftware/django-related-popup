5298a571280b996f43b033ebca54ec75ac10be78
Sævar Öfjörð Magnússon <saevar@overcast.io>
2014-08-22 13:48:14 +0000

    Update package name

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup 
 
 setup(
-    name='django-tekextensions',
-    version=__import__('tekextensions').__version__,
+    name='django-related-popup',
+    version=__import__('related_popup').__version__,
     description='A set of re-usable widgets and commands for django',
     long_description='',
     author='John Anderson',
     author_email='sontek@gmail.com',
-    url='http://github.com/sontek/django-tekextensions',
-    download_url='http://github.com/sontek/django-tekextensions',
+    url='http://github.com/overcastsoftware/django-related-popup',
+    download_url='http://github.com/overcastsoftware/django-related-popup',
     license='BSD',
-    packages=['tekextensions'],
+    packages=['related_popup'],
     classifiers = [
             'Framework :: Django',
             'License :: OSI Approved :: BSD License',
