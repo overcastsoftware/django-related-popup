da14cca50559b45208ed0f2b5ead4f6acfcd3f97
Sævar Öfjörð Magnússon <saevar@overcast.io>
2014-08-22 14:07:27 +0000

    Fix imports

--- setup.py
+++ setup.py
@@ -3,23 +3,21 @@ try:
 except ImportError: 
     from distutils.core import setup 
 
-from setuptools import find_packages
-
 setup(
     name='django-related-popup',
     version=__import__('related_popup').__version__,
     description='A set of re-usable widgets and commands for django',
     long_description='',
-    author='John Anderson',
-    author_email='sontek@gmail.com',
+    author='Overcast Software',
+    author_email='team@overcast.io',
     url='http://github.com/overcastsoftware/django-related-popup',
     download_url='http://github.com/overcastsoftware/django-related-popup',
-    license='BSD',
-    packages=find_packages(),
+    license='MIT',
+    packages=['related_popup', 'related_popup.templates'],
     include_package_data=True,
     classifiers = [
             'Framework :: Django',
-            'License :: OSI Approved :: BSD License',
+            'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
     ],
